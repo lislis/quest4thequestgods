3497826343e86140d76953144ec33c89488e5f86
lislis <mail@lislis.de>
2023-09-03 13:13:53 +0200

    tweaks

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
      },
      created() {
          this.time = new Date();
-         this.time.setSeconds(this.time.getSeconds() + 30);
+         this.time.setSeconds(this.time.getSeconds() + 180);
          this.timer = useTimer(this.time, false);
 
          //console.log(this.state)
@@ -47,7 +47,7 @@
          },
          restartQuest() {
              this.time = new Date();
-             this.time.setSeconds(this.time.getSeconds() + 30);
+             this.time.setSeconds(this.time.getSeconds() + 180);
 
              this.state.state = 0;
              this.state.score = 0;
@@ -86,18 +86,44 @@
 
 <template>
     <div v-if="state.state == 0">
-        <h1>Quests 4 the quest gods</h1>
+        <h1>Quests for the Questgods!</h1>
+        <p>Collect as many quest items as possible to offer them to the Questgods!</p>
         <button @click="startQuest">Start Quest</button>
     </div>
     <div v-if="state.state == 1">
+        <h2>Questing!</h2>
         <p><span>You have </span><span>{{timer.minutes}}</span>:<span>{{timer.seconds}}</span><span> left.</span></p>
         <p>You've quested {{ state.score }} items.</p>
-        {{ state.guess }}
-        <p>Your next quest: <strong>{{state.current}}</strong></p>
+        <p>Your next quest item is <strong>{{state.current}}</strong></p>
         <form @submit="checkIfCorrect" ><input type="text" name="item" autofocus v-model="state.guess" /></form>
     </div>
     <div v-if="state.state == 2">
+        <h2>Your quest is over!</h2>
         <p>You offered {{ state.score }} items to the quest gods.</p>
         <button @click="restartQuest()">New Quest</button>
     </div>
 </template>
+
+<style>
+
+ body {
+     font-family: sans-serif;
+     background-color: mintcream;
+     color: dimgray;
+ }
+
+ button {
+     background-color: dimgray;
+     color: white;
+     border: none;
+     padding: 0.8em 1em 0.6em;
+     font: inherit;
+     border-radius: 6px;
+     cursor: pointer;
+ }
+ button:hover,
+ button:active {
+     background-color: black;
+ }
+
+</style>
